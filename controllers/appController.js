a98c92332643fb466888cd7f9b17631d1086f236
Syf-Almjd <syfalmjd11@gmail.com>
2025-10-05 23:47:53 +0200

    new requirment

--- controllers/appController.js
+++ controllers/appController.js
@@ -10,10 +10,10 @@ const validateApp = (req, res) => {
   const allowedApps = process.env.ALLOWED_APPS.split(',');
 
   if (!allowedApps.includes(appName)) {
-    return res.status(400).json({ error: false });
+    return res.status(400).json({ isAppVaild: false });
   }
 
-  res.status(200).json({ message: true });
+  res.status(200).json({ isAppVaild: true });
 };
 
 module.exports = {
