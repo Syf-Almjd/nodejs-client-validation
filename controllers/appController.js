1f7cb5deb57190735b837db6326642ceebe49124
Syf-Almjd <syfalmjd11@gmail.com>
2025-10-06 00:02:35 +0200

    add client vaildation feature

--- controllers/appController.js
+++ controllers/appController.js
@@ -10,7 +10,7 @@ const validateApp = (req, res) => {
   const allowedApps = process.env.ALLOWED_APPS.split(',');
 
   if (!allowedApps.includes(appName)) {
-    return res.status(400).json({ isAppVaild: false });
+    return res.status(200).json({ isAppVaild: false });
   }
 
   res.status(200).json({ isAppVaild: true });
